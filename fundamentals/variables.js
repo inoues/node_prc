27fd41df2556e5184ab9a04665aef2362ec48e9d
inoues <psychobmx@gmail.com>
2020-07-16 23:33:48 +0900

    memo variables

---
+++ fundamentals/variables.js
@@ -0,0 +1,56 @@
+/** https://javascript.info/variables */
+"use strict";
+
+console.log('some code');
+
+let messege;
+messege = 'Hello';
+
+console.log(messege);
+
+let $ = 1;
+let _ = 2;
+console.log($ + _);
+
+const hisBirthday = '1990.10.10';
+// hisBirthday = '2020.7.10';
+
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_ORANGE;
+console.log(color);
+
+const bigInt = 1234567890123456789012345678901234567890n;
+
+let name = "abc";
+
+console.log(`${name}`);
+console.log('${name}');
+
+let age = null;
+
+console.log(age);
+
+let feet;
+
+console.log(feet);
+
+console.log(typeof 0);
+console.log(typeof console);
+
+console.log(String(1234));
+console.log(Number("12444"));
+
+console.log(Number(null));
+console.log(Number(1 > 0));
+
+console.log(Boolean(0));
+console.log(Boolean(''));
+
+console.log(4 ** (1/2));
+
+console.log( '2' > 1 ); 
+console.log( '01' == 1 );
+
+result = a || b;
